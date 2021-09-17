aafda2e5c9f9aeb2e820ff4f6bf6aeb5bbfee943
Liwo85 <w.liboska@wp.pl>
2021-09-17 12:19:49 +0200

    First message

---
+++ src/main/java/pl/sdacademy/FinalDesignProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.sdacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalDesignProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalDesignProjectApplication.class, args);
+    }
+
+}
