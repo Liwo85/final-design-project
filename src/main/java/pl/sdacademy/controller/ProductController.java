6ebedce668fdcd93a41baa15d3e1be8079e334f3
Liwo85 <w.liboska@wp.pl>
2021-09-18 17:58:41 +0200

    2. Weryfikacja kodu

--- src/main/java/pl/sdacademy/controller/ProductController.java
+++ src/main/java/pl/sdacademy/controller/ProductController.java
@@ -2,7 +2,7 @@ package pl.sdacademy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.sdacademy.model.Product;
 import pl.sdacademy.repository.ProductRepository;
 
@@ -18,9 +18,15 @@ public class ProductController {
     }
 
     @GetMapping("/page")
-    public String example4(Model model){
+    public String showAllProducts(Model model){
         List<Product> products = productRepository.findAll();
         model.addAttribute("products", products);
         return "page";
     }
+    @PostMapping("/pageAdd")
+    public String addProduct(@ModelAttribute("product") Product product){
+        productRepository.save(product);
+        return "/page_add";
+    }
+
 }
