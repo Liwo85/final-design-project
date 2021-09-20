4f1053f40d79efdb764341a347ab8de2c0bc2a81
Liwo85 <w.liboska@wp.pl>
2021-09-20 14:25:02 +0200

    2. Poprawiony find by id

--- src/main/java/pl/sdacademy/controller/ProductController.java
+++ src/main/java/pl/sdacademy/controller/ProductController.java
@@ -16,7 +16,6 @@ public class ProductController {
     private final ProductRepository productRepository;
 
 
-
     public ProductController(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
@@ -44,5 +43,20 @@ public class ProductController {
         ModelAndView modelAndView = new ModelAndView("single_product_page");
         modelAndView.addObject("product", productRepository.getById(identification));
         return modelAndView;
+    }
+
+    @DeleteMapping(path = "/mainPage/{identifier}")
+    public void deleteProductById(@PathVariable Integer identifier) {
+        productRepository.deleteById(identifier);
+    }
+
+    @PostMapping("/mainPage/edit/{identifier}")
+    public void editProductById(@RequestBody Product product, @PathVariable Integer identifier) {
+        List<Product> products = productRepository.findAll();
+        int index = product.getId() - 1;
+
+        products.get(index).setTitle(product.getTitle());
+        products.get(index).setDescription(product.getDescription());
+        products.get(index).setPrice(product.getPrice());
     }
 }
