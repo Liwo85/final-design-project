e221178572636739fd29e91d9e56db37079dc0c6
tomasz-krysicki <tomasz.krysicki@task.com.pl>
2021-09-20 12:47:05 +0200

    Day 2 - find by Id added

--- src/main/java/pl/sdacademy/controller/ProductController.java
+++ src/main/java/pl/sdacademy/controller/ProductController.java
@@ -3,15 +3,19 @@ package pl.sdacademy.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import pl.sdacademy.model.Product;
 import pl.sdacademy.repository.ProductRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class ProductController {
 
     private final ProductRepository productRepository;
+
+
 
     public ProductController(ProductRepository productRepository) {
         this.productRepository = productRepository;
@@ -33,5 +37,22 @@ public class ProductController {
     public String addProduct(@ModelAttribute("product") Product product) {
         productRepository.save(product);
         return "page_add_result";
+    }
+
+    public static Product getProductById(Integer identification) {
+        List<Product> products = new ArrayList<>();
+        for (Product product : products) {
+            if (product.getId().equals(identification)) {
+                return product;
+            }
+        }
+            return null;
+    }
+
+    @GetMapping("/mainPage/{identification}")
+    public ModelAndView productDetails(@PathVariable Integer identification) {
+        ModelAndView modelAndView = new ModelAndView("single_product_page");
+        modelAndView.addObject("product", ProductController.getProductById(identification));
+        return modelAndView;
     }
 }
