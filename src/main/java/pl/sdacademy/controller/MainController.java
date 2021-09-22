7bb2fa3541e70cd34271af055ec489fdbf710d19
Łukasza Abramczyk <abramspl@users.noreply.github.com>
2021-09-22 15:10:01 +0200

    dodanie strony startowej index.html, paska navi, małe poprawki w plikach HTML, dodanie klasy MainController

---
+++ src/main/java/pl/sdacademy/controller/MainController.java
@@ -0,0 +1,14 @@
+package pl.sdacademy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/index")
+    public String homePage() {
+        return "index";
+    }
+
+}
